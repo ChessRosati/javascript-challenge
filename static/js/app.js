8d97d478ff6379a961865937231c15a9d2b1c92e
ChessRosati <chessrosati@gmail.com>
2020-09-12 23:57:27 -0400

    basics

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,14 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+
+data.forEach((ufos) => {
+    var row = tbody.append("tr"); // Add a row to the table for each iteration
+    Object.entries(ufos).forEach(([key, value]) => {
+        var cell = row.append("td"); // Make a new cell for each entry
+        cell.text(value); // Fill in cell values
+    });
+});
+
